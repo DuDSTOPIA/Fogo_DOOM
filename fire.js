e18a07fce34d6169677973b568d19fd0b5cb9f96
DuDSTOPIA <dudscpa@hotmail.com>
2021-02-19 16:28:18 -0300

    Iniciando tabela de seis celulas com valor zero

--- fire.js
+++ fire.js
@@ -1,9 +1,18 @@
-function start() {
+const firePixelsArray = []
+const fireWidth = 2
+const fireHeight = 3
 
+function start() {
+    createFireDataStructure()
+    console.log()
 }
 
 function createFireDataStructure() {
+    const numberOfPixels = fireWidth * fireHeight
 
+    for(let i = 0; i < numberOfPixels; i++) {
+        firePixelsArray[i] = 0
+    }
 }
 
 function calculateFirePropagation() {
