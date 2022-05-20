2ada7b772eac17c7ad224eb4b8051c0a9ea8a80c
Eduardo Domiciano <dudscpa@hotmail.com>
2022-05-20 04:52:45 -0300

    Pagina finalizada, adicionado botões de + e - intensidade.

--- fire.js
+++ fire.js
@@ -2,15 +2,16 @@ const firePixelsArray = []
 const fireWidth = 70
 const fireHeight = 40
 const fireColorsPalette = [{ "r": 7, "g": 7, "b": 7 }, { "r": 31, "g": 7, "b": 7 }, { "r": 47, "g": 15, "b": 7 }, { "r": 71, "g": 15, "b": 7 }, { "r": 87, "g": 23, "b": 7 }, { "r": 103, "g": 31, "b": 7 }, { "r": 119, "g": 31, "b": 7 }, { "r": 143, "g": 39, "b": 7 }, { "r": 159, "g": 47, "b": 7 }, { "r": 175, "g": 63, "b": 7 }, { "r": 191, "g": 71, "b": 7 }, { "r": 199, "g": 71, "b": 7 }, { "r": 223, "g": 79, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 223, "g": 87, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 95, "b": 7 }, { "r": 215, "g": 103, "b": 15 }, { "r": 207, "g": 111, "b": 15 }, { "r": 207, "g": 119, "b": 15 }, { "r": 207, "g": 127, "b": 15 }, { "r": 207, "g": 135, "b": 23 }, { "r": 199, "g": 135, "b": 23 }, { "r": 199, "g": 143, "b": 23 }, { "r": 199, "g": 151, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 159, "b": 31 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 167, "b": 39 }, { "r": 191, "g": 175, "b": 47 }, { "r": 183, "g": 175, "b": 47 }, { "r": 183, "g": 183, "b": 47 }, { "r": 183, "g": 183, "b": 55 }, { "r": 207, "g": 207, "b": 111 }, { "r": 223, "g": 223, "b": 159 }, { "r": 239, "g": 239, "b": 199 }, { "r": 255, "g": 255, "b": 255 }]
+let intensity = 16
 
 function start() {
     createFireDataStructure()
-    createFireSource()
+    createFireSource(intensity)
     renderFire()
    
     setInterval(calculateFirePropagation, 50)
 }
-
+// analiza o tamanho da tabela e adiciona o valor para saber a intensidade. A intensidade é um numero de 0 a 36.
 function createFireDataStructure() {
     const numberOfPixels = fireWidth * fireHeight
 
@@ -18,7 +19,7 @@ function createFireDataStructure() {
         firePixelsArray[i] = 0
     }
 }
-
+// Analisa cada pixel das colunas para saber quais a intensidade de cada coluna e cada linha
 function calculateFirePropagation() {
     for(let column = 0; column < fireWidth; column ++) {
         for (let row = 0; row < fireHeight; row++) {
@@ -29,20 +30,21 @@ function calculateFirePropagation() {
     }
     renderFire()
 }
-
+// define o valor de intensidade de cada pixel
 function updateFireIntensityPerPixel(currentPixelIndex) {
-    const belowPixelIndex = currentPixelIndex + fireWidth
 
+    const belowPixelIndex = currentPixelIndex + fireWidth
     if (belowPixelIndex >= fireWidth * fireHeight) {
         return
     }
     const decay = Math.floor(Math.random() * 3)
     const belowPixelFireIntensity = firePixelsArray[belowPixelIndex]
-    const newFireIntensity = belowPixelFireIntensity - decay >= 0 ? belowPixelFireIntensity - decay : 0
+    const newFireIntensity = 
+            belowPixelFireIntensity - decay >= 0 ? belowPixelFireIntensity - decay : 0
 
     firePixelsArray[currentPixelIndex - decay] = newFireIntensity
 }
-
+// Cria a tabela e colore os pixel de acordo com oa valores de intensidade de cada um
 function renderFire() {
     const debug = false
     let html = '<table cellpadding=0 cllspacing=0'
@@ -74,16 +76,48 @@ function renderFire() {
     document.querySelector('#fireCanvas').innerHTML = html
 }
 
-function createFireSource(power) {
+//define a cor correta para iniciar o fogo a partir do ultimo bloco de cada coluna
+function createFireSource(intensity) {
+    
+    intensity = intensity
     for (let column = 0; column <= fireWidth; column++) {
         const overflowPixelIndex = fireWidth * fireHeight
         const pixelIndex = (overflowPixelIndex - fireWidth) + column
-        
-        let power = 25;
-        firePixelsArray[pixelIndex] = power;
+                
+        firePixelsArray[pixelIndex] = intensity;
     }
 }
 
+// Botão para aumentar o fogo
+var buttonMoreFire = document.querySelector("#moreFire");
+
+buttonMoreFire.onclick = function() {
+    event.preventDefault();
+    if(intensity < 36) {
+        intensity = intensity + 2
+        createFireSource(intensity)
+    }else if(intensity > 36) {
+        return intensity = 36;
+    }
+    console.log(intensity)
+    
+    
+}
+
+// Botão para diminuir o fogo
+var buttonLessFire = document.querySelector("#lessFire");
+
+buttonLessFire.onclick = function() {
+    event.preventDefault();
+    
+    if(intensity > 1 ) {
+        intensity = intensity - 2;
+        createFireSource(intensity)
+    }else if(intensity < 2) {
+        return intensity = 2;
+    }
+    console.log(intensity)
+}
 
 
 start()
